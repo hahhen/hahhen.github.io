45329b49a8649203341aa70e37066dcad62e2c3a
hahhen <arthurmp2007@hotmail.com>
2023-07-27 20:37:23 -0300

    more responsivity + fake login system

--- forum/js/index.js
+++ forum/js/index.js
@@ -6,13 +6,13 @@ var feed = document.getElementById("feed")
 var recentFeed = document.getElementById("recent-feed")
 
 retractBtn.addEventListener("click", (e)=>{
-    if(retractBtn.textContent == "< Recolher"){
+    if(retractBtn.textContent == " Recolher"){
         sectionsTit.style.display = "none"
         sectionsCol.classList = "col-1"
         sectionsMap.style.display = "none"
         feed.classList = "col-7 feed"
         recentFeed.classList = "col-4 recent-feed"
-        retractBtn.textContent = "> Expandir"
+        retractBtn.innerHTML = 'Expandir <i class="fa-solid fa-caret-right"></i>'
     }
     else{
         sectionsTit.style.display = "block"
@@ -20,6 +20,30 @@ retractBtn.addEventListener("click", (e)=>{
         sectionsMap.style.display = "block"
         feed.classList = "col-6 feed"
         recentFeed.classList = "col-3 recent-feed"
-        retractBtn.textContent = "< Recolher"
+        retractBtn.innerHTML = '<i class="fa-solid fa-caret-left"></i> Recolher'
     }
 })
+
+var loginBtn = document.getElementById("login-btn")
+var accountArea = document.getElementById("account-area")
+var profAreaCont = '<div id="profile-area"><img id="pfp" src="https://i.pinimg.com/1200x/0d/76/eb/0d76eb00b206c6add63521f738c8bc0c.jpg" width="40" alt="Profile picture" class="pfp me-2 rounded-circle"><i id="p_dropdown" class="fa-solid fa-caret-down" style="color: #ffffff;"></i></div>'
+
+loginBtn.addEventListener("click", (e)=>{
+    accountArea.innerHTML = profAreaCont
+    accountArea.addEventListener("click", (e)=>{
+        if(e.target.id == "profile-area" || e.target.id == "pfp" || e.target.id == "p_dropdown"){
+            if (accountArea.innerHTML == profAreaCont){
+                accountArea.innerHTML ='<div class="d-flex flex-column align-items-end"> <div id="profile-area"><img id="pfp" src="https://i.pinimg.com/1200x/0d/76/eb/0d76eb00b206c6add63521f738c8bc0c.jpg" width="40" alt="Profile picture" class="pfp me-2 rounded-circle"><i id="p_dropdown" class="fa-solid fa-caret-up" style="color: #ffffff;"></i></i></div> <div id="account-info" class="account-info"> <span>Minha conta</span> <span>Sair</span> </div> </div>'
+            }
+            else{
+                accountArea.innerHTML = profAreaCont
+            }
+        }
+    })
+    document.addEventListener("click", (e)=>{
+        if(e.target.id != "profile-area" && e.target.id != "pfp" && e.target.id != "p_dropdown"){
+            accountArea.innerHTML = profAreaCont
+        }
+    })
+})
+
